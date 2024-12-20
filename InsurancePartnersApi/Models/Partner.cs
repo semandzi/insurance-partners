a49b81351bce13eb6aeedac76663a74dbfbc5204
Senad <senad.mandzic1984@gmail.com>
2024-12-20 15:49:20 +0100

    partner model

---
+++ InsurancePartnersApi/Models/Partner.cs
@@ -0,0 +1,18 @@
+﻿namespace InsurancePartnersApi.Models
+{
+    public class Partner
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public long PartnerNumber { get; set; }
+        public string? CroatianPIN { get; set; }
+        public int PartnerTypeId { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public string CreatedByUser { get; set; }
+        public bool IsForeign { get; set; }
+        public string ExternalCode { get; set; }
+        public char Gender { get; set; }
+    }
+}
