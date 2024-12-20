01f38ff86ae7d9ee8761e3cdd2223c6b87739a49
Senad <senad.mandzic1984@gmail.com>
2024-12-20 15:49:31 +0100

    policy model

---
+++ InsurancePartnersApi/Models/Policy.cs
@@ -0,0 +1,10 @@
+﻿namespace InsurancePartnersApi.Models
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string PolicyNumber { get; set; }
+        public decimal PolicyAmount { get; set; }
+        public int PartnerId { get; set; }
+    }
+}
