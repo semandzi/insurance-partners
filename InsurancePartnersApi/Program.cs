1576121a016cea20a9b95eeebbaa018d741bbca6
Senad <senad.mandzic1984@gmail.com>
2024-12-20 15:54:01 +0100

    added DI

--- InsurancePartnersApi/Program.cs
+++ InsurancePartnersApi/Program.cs
@@ -1,6 +1,20 @@
+using InsurancePartnersApi.Data;
+using InsurancePartnersApi.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);// Register IConfiguration for DI
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));// Register the GenericRepository
+builder.Services.AddScoped<SqlConnection>(provider =>
+{
+    return new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<DatabaseContext>();// Register DatabaseContext as a scoped service
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
