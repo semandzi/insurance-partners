3f7729debce3c10f5d0385de48c36d8b8ed281a2
Senad <senad.mandzic1984@gmail.com>
2024-12-27 17:47:03 +0100

    app.tsx

---
+++ insurancepartnersfrontend/src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import PartnerPage from './pages/PartnerPage'
+import AddPartnerPage from './pages/AddPartnerPage'
+
+
+
+const  App: React.FC = () => {
+     
+  return (
+      <>
+          <Router>
+            <Navbar></Navbar>
+              <>
+                  <Routes>
+                      <Route path='/' element={<PartnerPage />}></Route>
+                      <Route path='/add-partner' element={<AddPartnerPage/>}></Route>
+                  </Routes>
+              </>
+            <Footer></Footer>
+          </Router>
+    </>
+  )
+}
+
+export default App
