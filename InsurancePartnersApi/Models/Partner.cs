d890ea624412581c98ada6eaf9699d14e998cac3
Senad <senad.mandzic1984@gmail.com>
2024-12-27 17:25:06 +0100

    partnerModel prop change type form long to decimal

--- InsurancePartnersApi/Models/Partner.cs
+++ InsurancePartnersApi/Models/Partner.cs
@@ -6,7 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public long PartnerNumber { get; set; }
+        public decimal PartnerNumber { get; set; }
         public string? CroatianPIN { get; set; }
         public int PartnerTypeId { get; set; }
         public DateTime CreatedAtUtc { get; set; }
