29e1068dbc2aa2bfd23ae1f1cf5928fea5998e32
Senad <senad.mandzic1984@gmail.com>
2024-12-27 17:31:26 +0100

    partner list page

---
+++ insurancepartnersfrontend/src/pages/PartnerPage.tsx
@@ -0,0 +1,13 @@
+import PartnerList from "../components/PartnerList";
+
+
+const PartnersPage: React.FC = () => {
+    
+    return (
+        <>
+            <PartnerList></PartnerList>
+        </>  
+    )
+};
+
+export default PartnersPage;
